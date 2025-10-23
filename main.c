dc25ae5fb4d945996f81bacc0bd67a8ab6e6451d
leanghok <leanghokoeng5@gmail.com>
2025-10-23 16:15:37 +0700

    add keybindings and selecting entries

--- main.c
+++ main.c
@@ -3,8 +3,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define KEY_Q 113
+#define KEY_J 106
+#define KEY_K 107
+
+typedef struct {
+  char *name;
+  unsigned char type;
+} entry;
+
 DIR *cwd;
 struct dirent *de;
+entry entries[1024];
+int len = 0;
+int selected = 0;
+int input = 0;
 
 void init() {
   cwd = opendir(".");
@@ -14,29 +27,61 @@ void init() {
   }
 
   initscr();
-  raw();
+  cbreak();
   keypad(stdscr, TRUE);
   noecho();
+  curs_set(0);
 }
 
 void cleanup() {
-  refresh();
-  getch();
   closedir(cwd);
   endwin();
+}
+
+void getentries() {
+  while ((de = readdir(cwd)) != NULL) {
+    entries[len].name = de->d_name;
+    entries[len].type = de->d_type;
+    len++;
+  }
+}
+
+void drawentries() {
+  clear();
+  for (int i = 0; i < len; i++) {
+    if (i == selected) {
+      attron(A_REVERSE);
+    }
+
+    if (entries[i].type == DT_DIR) {
+      printw("%s/\n", entries[i].name);
+    } else {
+      printw("%s\n", entries[i].name);
+    }
+
+    attroff(A_REVERSE);
+  }
+
+  refresh();
 }
 
 int main() {
   init();
 
-  while ((de = readdir(cwd)) != NULL) {
-    if (de->d_type == DT_DIR) {
-      attron(A_REVERSE);
-      printw("%s\n", de->d_name);
-      attroff(A_REVERSE);
-      continue;
+  getentries();
+  drawentries();
+
+  while (input != KEY_Q) {
+    if (input == KEY_J && selected < len - 1) {
+      selected++;
+      drawentries();
     }
-    printw("%s\n", de->d_name);
+    if (input == KEY_K && selected > 0) {
+      selected--;
+      drawentries();
+    }
+
+    input = getch();
   }
 
   cleanup();
